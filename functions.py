3f4ddff62e3cfb75f8bcb92871804380e14511e4
Tim Sherratt <tim@discontents.com.au>
2017-01-12 20:23:46 +1100

    Code and data for RecordSearch functions

---
+++ functions.py
@@ -0,0 +1,66 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+import pprint
+import json
+
+
+def harvest_functions():
+    functions = []
+    subfunc = {}
+    function = {}
+    r = requests.get('http://recordsearch.naa.gov.au/manual/Provenance/SummaryCRSThes.htm')
+    soup = BeautifulSoup(r.text, 'html5lib')
+    for row in soup.find_all('tr'):
+        cells = row.find_all('td')
+        try:
+            if not cells[0].find('p').string.strip() == 'NT':
+                if function:
+                    functions.append(function)
+                if subfunc:
+                    try:
+                        function['narrower'].append(subfunc)
+                    except KeyError:
+                        function['narrower'] = []
+                        function['narrower'].append(subfunc)
+                    subfunc = {}
+                function = {}
+                function['name'] = cells[0].get_text(' ', strip=True).encode('utf-8')
+                scope = cells[1].get_text(strip=True).replace('SCOPE NOTE ', '')
+                function['scope'] = re.sub(r'\s+', ' ', scope)
+            else:
+                for para in cells[1].find_all('p'):
+                    child = para.get_text(' ', strip=True)
+                    child = re.sub(r' ([a-z]{1})\b', r'\1', child)
+                    if child[:2] not in ['NT', 'BT']:
+                        if subfunc:
+                            try:
+                                function['narrower'].append(subfunc)
+                            except KeyError:
+                                function['narrower'] = []
+                                function['narrower'].append(subfunc)
+                        subfunc = {}
+                        subfunc['name'] = child
+                    else:
+                        try:
+                            subfunc['narrower'].append(child[2:].strip())
+                        except KeyError:
+                            subfunc['narrower'] = []
+                            subfunc['narrower'].append(child[2:].strip())
+        except AttributeError:
+            pass
+    with open('data/functions.txt', 'wb') as text_file:
+        for function in functions:
+            print '{}'.format(function['name'])
+            text_file.write('{}\n'.format(function['name']))
+            if 'narrower' in function:
+                for subf in function['narrower']:
+                    print '  - {}'.format(subf['name'])
+                    text_file.write('  - {}\n'.format(subf['name']))
+                    if 'narrower' in subf:
+                        for subsubf in subf['narrower']:
+                            print '    - {}'.format(subsubf)
+                            text_file.write('    - {}\n'.format(subsubf))
+    with open('data/functions.json', 'wb') as json_file:
+        json.dump(functions, json_file, indent=4)
+
